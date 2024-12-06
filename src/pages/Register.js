0281db2ee836d45aab85793e825950d3e3cbff05
dhruv0485 <dhruvmehta.2004@gmail.com>
2024-12-06 13:49:55 +0530

    changes made

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,7 +6,7 @@ import '../../src/css/register.css';
 
 async function SubmitRegisterForm(RegisterData) {
     try{
-        const API_URL = process.env.REACT_APP_API_URL || 'https://magicplate-admin.vercel.app/'
+        const API_URL = process.env.REACT_APP_API_URL || 'https://magicplate-admin.vercel.app'
         const response = await fetch(`${API_URL}/newRegister`,{
             method:'POST',
             headers: {
